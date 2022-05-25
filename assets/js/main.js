b28b94f49590810eef0d72220d9d58eb3c32f89d
Toby Leftly <tobyleftly@gmail.com>
2022-05-25 10:17:02 -0400

    fix for nav.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,43 @@
-$(function() {
-  
-  $(document).ready(function(){
-    $('#banner').slick({
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      fade: true,
-      dots: true,
-      arrows: false,
-    })
+$(document).ready(function(){
+
+  const map = document.getElementById("map")
+  const navTrigger = document.getElementById('navToggle')
+  const navClose = document.getElementById('navClose')
+
+  $('#banner').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+    dots: true,
+    arrows: false,
   })
 
-  const myLatLng = { lat: 51.7104978, lng: -0.6117067000000134 };
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: myLatLng,
-  });
-  new google.maps.Marker({
-    position: myLatLng,
-    map,
-    title: "Hello World!",
-  });
-  
+  if (map) {
+    const myLatLng = { lat: 51.7104978, lng: -0.6117067000000134 };
 
-})
+    const mapElem = new window.google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: myLatLng,
+    })
+
+    new google.maps.Marker({
+      position: myLatLng,
+      mapElem,
+      title: "Hello World!",
+    })
+  }
+
+  navTrigger.addEventListener('click', function() {
+    const mobileNav = document.getElementById('mobileNav')
+    mobileNav.classList.add('active')
+  })
+
+  navClose.addEventListener('click', function() {
+    const mobileNav = document.getElementById('mobileNav')
+    mobileNav.classList.remove('active')
+  })
+
+});
